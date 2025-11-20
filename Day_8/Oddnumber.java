97b11f3c64709faab0cefe0fa8301c61656cfacd
simon-hub11 <simonhub01@gmail.com>
2025-11-20 15:23:20 +0545

    updated

---
+++ Day_8/Oddnumber.java
@@ -0,0 +1,12 @@
+package Day_8;
+
+public class Oddnumber {
+    public static void main(String[] args) {
+        for(int i=1; i<=20; i++){
+            int odd=2*i-1;
+            System.out.println(odd);
+
+        }
+    }
+    
+}
